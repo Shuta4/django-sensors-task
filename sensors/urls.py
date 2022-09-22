3159f3d961fdc541db87af0affac7a1ff9162fda
Shuta4 <skirdin.nik@gmail.com>
2022-09-22 11:49:05 +0300

    Add /sensors/<pk>/reports endpoint

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework_extensions.routers import ExtendedSimpleRouter
 
 from . import views
 
-router = DefaultRouter()
-router.register(r'sensors', views.SensorViewSet, basename='sensor')
+router = ExtendedSimpleRouter()
+router.register(r'sensors', views.SensorViewSet, basename='sensor')\
+    .register(r'reports', views.ReportViewSet, basename='sensors-report',
+              parents_query_lookups=['sensor_reports'])
 router.register(r'reports', views.ReportViewSet, basename='report')
 
 urlpatterns = [
--- sensors/views.py
+++ sensors/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
+from rest_framework_extensions import mixins
 from .models import Sensor, Report
 from .serializers import SensorSerializer, ReportSerializer
 
 
-class SensorViewSet(viewsets.ModelViewSet):
+class SensorViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
